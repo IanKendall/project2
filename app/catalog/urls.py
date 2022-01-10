099a36948545b0ffe64c128c478a2bacd5d424e4
IanKendall <iankendall0@gmail.com>
2022-01-10 13:54:53 -0600

    Generic Views built

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('wordstructures/', views.WordStructureView.as_view(), name='wordstructures'),
+    path('wordstructure/<int:pk>', views.WordStructureDetailView.as_view(), name='wordstructure-detail'),
 ]
